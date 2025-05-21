821555f6b4580e6a168e971fa410dea3ac42e09d
Mahak Pandey <mahak0312pandey@gmail.com>
2025-05-21 18:51:01 +0530

    Action is created and an practical example

--- Redux/Day22/index.js
+++ Redux/Day22/index.js
@@ -1,1 +1,10 @@
-console.log("Helloo from redux index.js")
+//Creation of Action
+
+const BUY_CAKE = 'BUY_CAKE';
+
+function buyCake () {
+    return {
+        type: BUY_CAKE,
+        info: 'First redux action'
+    }
+}
