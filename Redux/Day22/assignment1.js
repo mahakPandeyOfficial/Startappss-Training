98abefbd4506a71d291875c4a55da3bc45a5b104
Mahak Pandey <mahak0312pandey@gmail.com>
2025-05-23 18:25:52 +0530

    Async Action and middleware

--- Redux/Day22/assignment1.js
+++ Redux/Day22/assignment1.js
@@ -1,5 +1,9 @@
 const redux =  require('redux');
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers; //importing combineReducers from redux to combine multiple reducers
+const applyMiddleware = redux.applyMiddleware; //importing applyMiddleware from redux to apply middleware
+const reduxLogger = require('redux-logger'); //importing logger middleware for logging the actions and state changes
+const logger = reduxLogger.logger; //creating a logger middleware
 
 //Craetion of Counter Action
 const ADD = 'ADD';
@@ -46,18 +50,18 @@ function subtractReducer (state = initialState, action) {
 }
 
 //Combining Reducers
-const rootReducer = redux.combineReducers({
+const rootReducer = combineReducers({
     add: addReducer,
     subtract: subtractReducer,
 })
 
 
 //Creating a Store
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));   // rootReducer and applyMiddleware are passed as arguments to createStore function. applyMiddleware is used to apply middleware to the store. logger is a middleware which logs the actions and state changes in the console.
 
 console.log('Initial State', store.getState());
 //Subscribing to the Store
-const unsubscribe = store.subscribe(() => console.log('Updated State', store.getState()));
+const unsubscribe = store.subscribe(() => {}, {/*console.log('Updated State', store.getState())*/} );
 //Disptaching action
 store.dispatch(add());
 store.dispatch(add());
---
+++ Redux/Day22/asyncAction.js
@@ -0,0 +1,58 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+//State the initial state
+const initialState = {
+    loading: false,
+    data: [],
+    error: ''
+}
+
+//Action Types  
+const FETCH_DATA_REQUEST = 'FETCH_DATA_REQUEST';
+const FETCH_DATA_FAILURE = 'FETCH_DATA_FAILURE';
+const FETCH_DATA_SUCCESS = 'FETCH_DATA_SUCCESS';
+
+
+//Action Creators
+const fetchDataRequest = () => {
+    return {
+        type: FETCH_DATA_REQUEST
+    }
+}
+
+const fetchDataFailure = (error) => {
+    return {
+        type: FETCH_DATA_FAILURE,
+        payload: error                     //PAYLOAD:  It is a way to pass data from the action creator to the reducer, allowing the reducer to perform specific tasks based on the action type and payload
+    }
+}
+
+const fetchDataSuccess = (data) => {
+    return {
+        type: FETCH_DATA_SUCCESS,
+        payLoad: data
+    }
+}
+
+//Reducer  
+const reducer = (state = initialState, action) =>{
+    switch (action.type){
+        case FETCH_DATA_REQUEST: return {
+            loading: true
+        }
+
+        case FETCH_DATA_SUCCESS: return {
+            loading: false,
+            data: action.payload,
+            error: ''
+        }
+
+        case FETCH_DATA_FAILURE: return {
+            loading: false,
+            data: [],
+            error: action.payload,
+        }
+    }
+}
+
+const store = createStore(reducer);
