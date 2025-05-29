53f0f62b316693e4a9f8d44fdbf91eac3260a956
Mahak Pandey <mahak0312pandey@gmail.com>
2025-05-29 16:20:58 +0530

    Middleware added and Day 24 Updates

--- Redux/Day22/ShoppingCartApp/src/store.js
+++ Redux/Day22/ShoppingCartApp/src/store.js
@@ -1,6 +1,8 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import { composeWithDevTools } from "@redux-devtools/extension";
 import cartReducers from './reducers/cartReducers';
 
-const store = createStore(cartReducers);
+const store = createStore(cartReducers, composeWithDevTools(applyMiddleware(logger)));
 export default store;
 
