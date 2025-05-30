a12f25f140a2f2dc326155a9bc287befef2dd5de
Mahak Pandey <mahak0312pandey@gmail.com>
2025-05-30 17:48:21 +0530

    Home Component Addded in Day24

---
+++ Redux/Day24/ECommerceApp/src/components/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="hero">
+    <div className="card text-bg-dark text-black border-0">
+      <img src="./assets/heroBg.png" className="card-img" alt="background" height="600px" />
+      <div className="card-img-overlay d-flex justify-content-start align-items-center">
+        <div className="container text-start" style={{  maxWidth: '30%', marginLeft: '1000px', marginBottom: '190px'}}>
+          <h5 className="card-title display-3 mb-0">YOUR STYLE. YOUR STORE</h5>
+          <p className="card-text lead fs-2">
+          "Bringing the mall to your screen, one click at a time."
+          </p>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  );
+};
+
+export default Home;
