25eb0cb18a292b00e260677493b900601c8c054c
mahakPandeyStartappss <mahak0312pandey@gmail.com>
2025-06-17 18:21:18 +0530

    Updates in EXpress Learning

---
+++ ExpressJS/app.js
@@ -0,0 +1,27 @@
+import express from 'express';
+
+const app = express();
+const PORT = 8000;
+
+// Middleware
+app.use((req, res, next) => {
+    console.log("This is first dummy middleware");
+    next();
+    
+});
+
+app.use((req, res, next ) => {
+    console.log("This is first dummy middleware");
+    next();
+});
+
+app.use("/", (req, res) => {
+
+})
+
+
+
+//Listener
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
